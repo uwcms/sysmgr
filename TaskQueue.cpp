uwcms/sysmgr

commit 80e320a9e620a059117e7839fe2fc63e30379db5

condition variable queueNotEmpty added schedule(...) sends signal in loop in run_until(): wait for signal or timeout

TaskQueue.cpp
@@ -1,4 +1,20 @@
+/*
+ *
+ * 	 TaskQueue.cpp
+ *
+ * 	 methods of sysmgr class TaskQueue    by  University of Wisconsin
+ *
+ *
+ * $Author$
+ * $Revision$
+ * $Date$
+ *
+ *
+ */
+
+
 #include <queue>
+#include <iostream>
 #include <time.h>
 #include <unistd.h>
 
@@ -10,6 +26,7 @@ TaskQueue::taskid_t TaskQueue::schedule(time_t when, callback<void> cb, void *cb
 
 	TaskQueue::taskid_t id = this->next_id++;
 	queue.push(ScheduledTask(id, when, cb, cb_data));
+	pthread_cond_signal( &queueNotEmpty );
 	return id;
 }
 
@@ -32,9 +49,20 @@ int TaskQueue::run_until(time_t stop_after)
 {
 	int processed = 0;
 	time_t now = time(NULL);
+	timespec dt;
+	dt.tv_sec = now + 1;
+	dt.tv_nsec = 0;
+
 	while (1) {
 		scope_lock llock(&this->lock);
 
+		do
+		{
+			dt.tv_sec = time(NULL) + 1;
+			pthread_cond_timedwait( &this->queueNotEmpty, &this->lock, &dt );
+		} while ( queue.empty() );
+
+		now = time(NULL);
 		ScheduledTask e = queue.top();
 		if (e.when <= now) {
 			queue.pop();
@@ -42,16 +70,11 @@ int TaskQueue::run_until(time_t stop_after)
 			llock.unlock();
 			e.cb(e.cb_data);
 			llock.lock();
-
 			processed++;
 		}
 		else {
 			if (stop_after && stop_after < now)
 				return processed;
-			else {
-				sleep(1);
-				now = time(NULL);
-			}
 		}
 	}
 }

commit 092a2dc0138c3fdccf362bb7b1d42b19f83f5699

Fix edge cases seemingly broken in 80e320a9

TaskQueue.cpp
@@ -56,12 +56,6 @@ int TaskQueue::run_until(time_t stop_after)
 	while (1) {
 		scope_lock llock(&this->lock);
 
-		do
-		{
-			dt.tv_sec = time(NULL) + 1;
-			pthread_cond_timedwait( &this->queueNotEmpty, &this->lock, &dt );
-		} while ( queue.empty() );
-
 		now = time(NULL);
 		ScheduledTask e = queue.top();
 		if (e.when <= now) {
@@ -75,6 +69,9 @@ int TaskQueue::run_until(time_t stop_after)
 		else {
 			if (stop_after && stop_after < now)
 				return processed;
+
+			dt.tv_sec = now+1;
+			pthread_cond_timedwait(&this->queueNotEmpty, &this->lock, &dt);
 		}
 	}
 }