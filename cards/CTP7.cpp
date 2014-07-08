da9bfc1fbe44803f0ee609519f4430bc2355c7e6
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2014-07-08 14:54:38 -0500

    Correct APIVER types

--- cards/CTP7.cpp
+++ cards/CTP7.cpp
@@ -4,8 +4,8 @@
 #include "CTP7.h"
 
 extern "C" {
-	int APIVER = 1;
-	int MIN_APIVER = 1;
+	uint32_t APIVER = 1;
+	uint32_t MIN_APIVER = 1;
 	bool initialize_module(std::string config) { return true; };
 	Card *instantiate_card(Crate *crate, std::string name, void *sdrbuf, uint8_t sdrbuflen) {
 		if (name == "WISC CTP-7")
--- cards/UWCard.cpp
+++ cards/UWCard.cpp
@@ -5,8 +5,8 @@
 #include "UWCard.h"
 
 extern "C" {
-	int APIVER = 1;
-	int MIN_APIVER = 1;
+	uint32_t APIVER = 1;
+	uint32_t MIN_APIVER = 1;
 	bool initialize_module(std::string config) { return true; };
 	Card *instantiate_card(Crate *crate, std::string name, void *sdrbuf, uint8_t sdrbuflen) {
 		if (name.substr(0,4) == "WISC")
